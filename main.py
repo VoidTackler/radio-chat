ef34f14dd5d28a5046bc3ced4f19d9d071844e38
VoidTackler <133778502+VoidTackler@users.noreply.github.com>
2023-10-17 09:35:46 +0100

    Initial files for MakeCode project

---
+++ main.py
@@ -0,0 +1,28 @@
+def on_received_number(receivedNumber):
+    if receivedNumber == 0:
+        basic.show_leds("""
+            . . . . .
+            . . . . .
+            . # # # .
+            . . . . .
+            . . . . .
+            """)
+    if receivedNumber == 1:
+        basic.show_leds("""
+            . . . . .
+            . . . . .
+            . . # . .
+            . . . . .
+            . . . . .
+            """)
+radio.on_received_number(on_received_number)
+
+def on_button_pressed_a():
+    radio.send_number(0)
+input.on_button_pressed(Button.A, on_button_pressed_a)
+
+def on_button_pressed_b():
+    radio.send_number(1)
+input.on_button_pressed(Button.B, on_button_pressed_b)
+
+radio.set_group(1)
---
+++ main.ts
@@ -0,0 +1,27 @@
+radio.onReceivedNumber(function (receivedNumber) {
+    if (receivedNumber == 0) {
+        basic.showLeds(`
+            . . . . .
+            . . . . .
+            . # # # .
+            . . . . .
+            . . . . .
+            `)
+    }
+    if (receivedNumber == 1) {
+        basic.showLeds(`
+            . . . . .
+            . . . . .
+            . . # . .
+            . . . . .
+            . . . . .
+            `)
+    }
+})
+input.onButtonPressed(Button.A, function () {
+    radio.sendNumber(0)
+})
+input.onButtonPressed(Button.B, function () {
+    radio.sendNumber(1)
+})
+radio.setGroup(1)
